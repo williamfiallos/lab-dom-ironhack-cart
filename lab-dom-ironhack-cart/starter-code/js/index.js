c9da51d7290ed73e4f4be47138f628e76647f9ef
williamfiallos <40927748+williamfiallos@users.noreply.github.com>
2018-11-19 15:44:31 -0500

    Update HTML file
    Trying to comprehend and work on JS file.

---
+++ lab-dom-ironhack-cart/starter-code/js/index.js
@@ -0,0 +1,57 @@
+function deleteItem(e){
+
+}
+
+function getPriceByProduct(itemNode){
+  let unitPrices = [...document.getElementsByClassName("product-price")];
+
+  const allPrices = unitPrices.map(oneDiv => {
+    return +oneDiv.innerHTML.substr
+  })
+}
+
+function updatePriceByProduct(productPrice, index){
+
+}
+
+function getTotalPrice() {
+
+}
+
+function createQuantityInput(){
+
+}
+
+function createDeleteButton(){
+
+}
+
+function createQuantityNode(){
+
+}
+
+function createItemNode(dataType, itemData){
+
+}
+
+function createNewItemRow(itemName, itemUnitPrice){
+
+}
+
+function createNewItem(){
+
+}
+
+window.onload = function(){
+  var calculatePriceButton = document.getElementById('calc-prices-button');
+  var createItemButton = document.getElementById('new-item-create');
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+
+  calculatePriceButton.onclick = getTotalPrice;
+  createItemButton.onclick = createNewItem;
+
+  for(var i = 0; i<deleteButtons.length ; i++){
+    deleteButtons[i].onclick = deleteItem;
+  }
+};
+
